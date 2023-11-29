5b2ca7efc86b6e37a4ffbc2781bbed290637471b
Barr0ca <iandearaujogalvao@gmail.com>
2023-11-29 12:05:30 -0300

    Alteração no método POST, agora está utilizando AXIOS para fazer a requisição via API;
    A identação do código foi alterada (ficou mais organizada)

--- Controller.js
+++ Controller.js
@@ -1,30 +1,57 @@
-const express=require('express');
-const cors=require('cors');
-const bodyParser=require('body-parser');
-const models=require('./models/index')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const models = require("./models/index");
+const axios = require("axios");
+const config = require("./config/config.json");
 
-const app=express();
+const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-let empresas=models.Empresas;
+// let empresas = models.Empresas;
 
-app.post('/Empresa',async (req,res)=>{
-    let reqs = await empresas.create({
-        nomeEmpresa: req.body.nomeEmpresa,
-        enderecoEmpresa: req.body.enderecoEmpresa,
-        whatsappEmpresa: req.body.whatsappEmpresa,
-        telefoneEmpresa: req.body.telefoneEmpresa,
-        imagensEmpresa: req.body.imagensEmpresa
+// app.post("/Empresa", async (req, res) => {
+//   let reqs = await empresas.create({
+//     nomeEmpresa: req.body.nomeEmpresa,
+//     enderecoEmpresa: req.body.enderecoEmpresa,
+//     whatsappEmpresa: req.body.whatsappEmpresa,
+//     telefoneEmpresa: req.body.telefoneEmpresa,
+//     imagensEmpresa: req.body.imagensEmpresa,
+//   });
+//   if (reqs) {
+//     res.send(JSON.stringify("Empresa cadastrada com sucesso!"));
+//   }
+// });
+
+app.post("/Empresa", async (req, res) => {
+  //const url = config.urlApi + "Empresa";
+  const url = config.urlWebApi + "Empresa";
+  const dados = {
+    nomeEmpresa: req.body.nomeEmpresa,
+    enderecoEmpresa: req.body.enderecoEmpresa,
+    whatsappEmpresa: req.body.whatsappEmpresa,
+    telefoneEmpresa: req.body.telefoneEmpresa,
+    imagensEmpresa: req.body.imagensEmpresa,
+  };
+
+  axios
+    .post(url, dados)
+    .then((response) => {
+      res.send(
+        JSON.stringify(
+          `Empresa ${response.data.nomeEmpresa} cadastrada com sucesso!`
+        )
+      );
+    })
+    .catch((error) => {
+      console.error("Erro na requisição para o backend:", error);
     });
-    if (reqs){
-        res.send(JSON.stringify('Empresa cadastrada com sucesso!'))
-    }
 });
 
-let port=process.env.PORT || 5188;
-app.listen(port,(req,res)=>{
-    console.log('Servidor Rodando!');
+let port = process.env.PORT || 1000;
+app.listen(port, (req, res) => {
+  console.log("Servidor Rodando!");
 });
 
 /*
